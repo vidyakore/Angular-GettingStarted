253754f62b886bfa0948956f5680034fe6bbf2f6
vidyakore <vidyakore3108@gmail.com>
2022-06-05 19:35:26 +0530

    Property Binding is done, images are shown using property binding

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'pm-products',  //so we can use it as a directive in any other component
+    templateUrl: './product-list.component.html'
+})
+ 
+export class ProductListComponent {
+pageTitle: string = 'Product List';
+imageWidth: number = 60;
+imageMargin: number = 40;
+products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
+
+
+}
